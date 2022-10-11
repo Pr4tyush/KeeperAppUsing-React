9aab4bb71b3a4e71898eb4339a408100cc279e80
Pr4tyush <kumarpratyushdss1999@gmail.com>
2022-10-11 23:23:22 +0530

    kepperApp

---
+++ build/precache-manifest.e4f90ef97795de9dd058250e748cd351.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f447940c0bb123b33063281c25bf3788",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91a8ce9d867e3d9aab4d",
+    "url": "/static/js/2.7358e7b1.chunk.js"
+  },
+  {
+    "revision": "6b2a5822dd54c0b59bf4",
+    "url": "/static/js/main.ebdbdc3b.chunk.js"
+  },
+  {
+    "revision": "2f55955791534fe0d08b",
+    "url": "/static/js/runtime-main.1048f588.js"
+  }
+]);
